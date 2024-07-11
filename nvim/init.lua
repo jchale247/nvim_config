78c875135134cc6d828769f31c9bcec6b56f0854
jchale247 <57643872+jchale247@users.noreply.github.com>
2024-07-10 21:05:11 -0500

    Add files via upload
    init commit

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require("joseph.plugins-setup")
+require("joseph.core.options")
+require("joseph.core.keymaps")
+require("joseph.core.colorscheme")
+require("joseph.plugins.comment")
+require("joseph.plugins.nvim-tree")
+require("joseph.plugins.lualine")
+require("joseph.plugins.telescope")
+require("joseph.plugins.nvim-cmp")
+
+
